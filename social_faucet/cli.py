8539578aeaf5df4f54873401229ef7c4dea17bf1
Daniel Perez <daniel@perez.sh>
2021-05-06 20:44:03 +0200

    Allow to customize port for HTTP control

--- social_faucet/cli.py
+++ social_faucet/cli.py
@@ -11,11 +11,23 @@ twitter_kovan_parser = subparsers.add_parser(
 )
 twitter_kovan_parser.add_argument("keywords", nargs="+")
 twitter_kovan_parser.add_argument("--db", required=True, help="Path to rate limit DB")
+twitter_kovan_parser.add_argument(
+    "--control-port",
+    type=int,
+    required=True,
+    help="HTTP port to use to control the process",
+)
 
 discord_kovan_parser = subparsers.add_parser(
     "discord-kovan-tokens", help="Runs Discord Kovan Faucet sending tokens"
 )
 discord_kovan_parser.add_argument("--db", required=True, help="Path to rate limit DB")
+discord_kovan_parser.add_argument(
+    "--control-port",
+    type=int,
+    required=True,
+    help="HTTP port to use to control the process",
+)
 
 
 def run():
@@ -26,9 +38,9 @@ def run():
         parser.error("no command provided")
 
     if args.command == "twitter-kovan":
-        runner.run_twitter_kovan_faucet(args.keywords, args.db)
+        runner.run_twitter_kovan_faucet(args.keywords, args.db, args.control_port)
     elif args.command == "discord-kovan-tokens":
-        runner.run_discord_tokens_kovan_faucet(args.db)
+        runner.run_discord_tokens_kovan_faucet(args.db, args.control_port)
 
 
 if __name__ == "__main__":
--- social_faucet/runner.py
+++ social_faucet/runner.py
@@ -14,14 +14,18 @@ from social_faucet.faucet_executor import FaucetExecutor
 from social_faucet.rate_limiter import RateLimiter
 
 
-def launch_control_app(rate_limiter: RateLimiter, faucet_executor: FaucetExecutor):
+def launch_control_app(
+    port: int, rate_limiter: RateLimiter, faucet_executor: FaucetExecutor
+):
     app.rate_limiter = rate_limiter
     app.faucet_executor = faucet_executor
-    app_thread = Thread(target=app.run, kwargs={"debug": False}, daemon=True)
+    app_thread = Thread(
+        target=app.run, kwargs={"port": port, "debug": False}, daemon=True
+    )
     app_thread.start()
 
 
-def run_faucet(web3: Web3, faucet: Faucet, db_path: str):
+def run_faucet(web3: Web3, faucet: Faucet, db_path: str, control_port: int):
     with dbm.open(db_path, "c") as db:
         rate_limiter = RateLimiter(db)
         transaction_builders = faucet.create_transaction_builders(web3)
@@ -33,22 +37,22 @@ def run_faucet(web3: Web3, faucet: Faucet, db_path: str):
             validators=validators,
         )
 
-        launch_control_app(rate_limiter, faucet_executor)
+        launch_control_app(control_port, rate_limiter, faucet_executor)
 
         faucet.listen(faucet_executor)
 
 
-def run_kovan_faucet(faucet: Faucet, db_path: str):
+def run_kovan_faucet(faucet: Faucet, db_path: str, control_port: int):
     from web3.auto.infura.kovan import w3
 
-    run_faucet(w3, faucet, db_path)
+    run_faucet(w3, faucet, db_path, control_port)
 
 
-def run_twitter_kovan_faucet(keywords: List[str], db_path: str):
+def run_twitter_kovan_faucet(keywords: List[str], db_path: str, control_port: int):
     faucet = TwitterKovanFaucet(keywords)
-    run_kovan_faucet(faucet, db_path)
+    run_kovan_faucet(faucet, db_path, control_port)
 
 
-def run_discord_tokens_kovan_faucet(db_path: str):
+def run_discord_tokens_kovan_faucet(db_path: str, control_port: int):
     faucet = DiscordMintTokensAsOwnerKovanFaucet()
-    run_kovan_faucet(faucet, db_path)
+    run_kovan_faucet(faucet, db_path, control_port)
